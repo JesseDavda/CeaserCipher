3557008f19e1085884d58539744c99c90ea276f5
Jesse <stratosphere1243@gmail.com>
2015-08-07 08:50:19 +0100

    added a decrypt function and a choice system

--- ceasercipher.cpp
+++ ceasercipher.cpp
@@ -4,18 +4,59 @@
 using namespace std;
 
 string encryptor(string word, int num);
+string decryptor(string word, int num);
 
 int main() {
-    string main_string;
+    string main_string, choice;
     int numOfSpaces;
 
-    cout << "Please enter a word you want encrypted" << endl;
-    cin >> main_string;
-    cout << "please enter the amount of spaces you want word to be encrypted by" << endl;
-    cin >> numOfSpaces;
+    cout << "Encrypt: 1" << endl;
+    cout << "Decrypt: 2" << endl;
+    cin >> choice;
 
-    cout << encryptor(main_string, numOfSpaces);
-    cout << " " << endl;
+    if(choice == "1") {
+
+        cout << "Please enter a word you want encrypted" << endl;
+        cin >> main_string;
+        cout << "please enter the amount of spaces you want word to be encrypted by" << endl;
+        cin >> numOfSpaces;
+
+        cout << encryptor(main_string, numOfSpaces);
+        cout << " " << endl;
+
+    } else if(choice == "2") {
+        cout << "Please enter a word you want decrypted" << endl;
+        cin >> main_string;
+        cout << "please enter the amount of spaces the word was ecrypted by" << endl;
+        cin >> numOfSpaces;
+
+        cout << decryptor(main_string, numOfSpaces);
+        cout << " " << endl;
+    }
+
+}
+
+string decryptor(string word, int num) {
+    string de_main = word;
+    int de_main_num = num, wordLength = word.length();
+
+    for(int i = 0; i <= wordLength; i++) {
+        char letter = de_main[i];
+        int unicodeLetter = letter;
+
+        int decryptedUnicode = unicodeLetter - de_main_num;
+
+        if(decryptedUnicode > 123) {    // if the encrypted value is out of the A-Z alphabet then bring it back
+            decryptedUnicode -= 26;
+        } else if(decryptedUnicode < 97) {
+            decryptedUnicode += 26;
+        }
+
+        char decryptedLetter = decryptedUnicode;
+        de_main[i] = decryptedLetter;
+    }
+
+    return de_main;
 }
 
 string encryptor(string word, int num) {
