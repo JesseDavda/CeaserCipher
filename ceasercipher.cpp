a107e450c97f8d926da7ef62f4ade077da2d32cf
Monty Anderson <monty.linux@gmail.com>
2015-08-12 21:48:21 +0100

    put functions in their own namespace

--- ceasercipher.cpp
+++ ceasercipher.cpp
@@ -27,7 +27,7 @@ int main() {
         cin >> numOfSpaces;
 
         file.open("ceaser.txt");
-        cout << encryptor(main_string, numOfSpaces);
+        cout << ceaser::encryptor(main_string, numOfSpaces);
         if(file.is_open()) {
             file << encryptor(main_string, numOfSpaces);
             file.close();
@@ -61,7 +61,7 @@ int main() {
                 cout << "please enter the amount of spaces the word was encrypted by: ";
                 cin >> numOfSpaces;
 
-                cout << decryptor(main_string, numOfSpaces) << endl;
+                cout << ceaser::decryptor(main_string, numOfSpaces) << endl;
 
             } else if(choice_two == "2") {
                 cout << "Please enter a word you want decrypted" << endl;
@@ -75,50 +75,4 @@ int main() {
         }
     }
 
-}
-
-string decryptor(string word, int num) {
-    string de_main = word;
-    int de_main_num = num, wordLength = word.length();
-
-    for(int i = 0; i <= wordLength; i++) {
-        char letter = de_main[i];
-        int unicodeLetter = letter;
-
-        int decryptedUnicode = unicodeLetter - de_main_num;
-
-        if(decryptedUnicode > 123) {    // if the encrypted value is out of the A-Z alphabet then bring it back
-            decryptedUnicode -= 26;
-        } else if(decryptedUnicode < 97) {
-            decryptedUnicode += 26;
-        }
-
-        char decryptedLetter = decryptedUnicode;
-        de_main[i] = decryptedLetter;
-    }
-
-    return de_main;
-}
-
-string encryptor(string word, int num) {
-    string main = word;
-    int main_num = num, wordLength = word.length();
-
-    for(int i = 0; i <= wordLength; i++) {
-        char letter = main[i];
-        int unicodeLetter = letter;
-
-        int encryptedUnicode = unicodeLetter + main_num;
-
-        if(encryptedUnicode > 123) {    // if the encrypted value is out of the A-Z alphabet then bring it back
-            encryptedUnicode -= 26;
-        } else if(encryptedUnicode < 97) {
-            encryptedUnicode += 26;
-        }
-
-        char encryptedLetter = encryptedUnicode;
-        main[i] = encryptedLetter;
-    }
-
-    return main;
 }
